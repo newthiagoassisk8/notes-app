54f942b48587ce9d4d859507f12f1a758bb37929
newthiagoassisk8 <assisthiago2303@gmail.com>
2025-11-22 09:14:36 -0300

    uso de variavel de ambiente

--- src/integrations/api.ts
+++ src/integrations/api.ts
@@ -1,5 +1,7 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
 export async function getNotes(items = 3, page = 1, tag?: string) {
-    const baseUrl = 'http://192.168.0.27:3000/api/notes';
+    const baseUrl = `${API_URL}/notes`;
     const params = new URLSearchParams({
         limit: items.toString(),
         page: page.toString(),
@@ -23,7 +25,8 @@ export async function getNotes(items = 3, page = 1, tag?: string) {
 
 export async function deleteNote(id: string) {
     try {
-        const res = await fetch(`http://192.168.0.27:3000/api/notes/${id}`, {
+        const baseUrl = `${API_URL}/notes`;
+        const res = await fetch(`${baseUrl}/${id}`, {
             method: 'DELETE',
         });
         if (!res.ok) {
