19345e14160f3f2763be84eb74e5a4b037365067
newthiagoassisk8 <assisthiago2303@gmail.com>
2025-12-11 22:34:24 -0300

    add missing files in order to reactquery works

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,22 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import { About } from './routes/about.tsx';
 import { Navbar } from './components/shared/navbar.tsx';
 import { NoteId } from './routes/note-id.tsx';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { NewNote } from './routes/new-note.tsx';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Navbar />
-        <Routes>
-            <Route index element={<App />} />
-            <Route path="about" element={<About />} />
-            <Route path="notes/:id" element={<NoteId />} />
-            <Route path="notes/new" element={<NewNote />} />
-        </Routes>
+
+        <QueryClientProvider client={queryClient}>
+            <Routes>
+                <Route index element={<App />} />
+                <Route path="about" element={<About />} />
+                <Route path="notes/:id" element={<NoteId />} />
+                <Route path="notes/new" element={<NewNote />} />
+            </Routes>
+        </QueryClientProvider>
     </BrowserRouter>
 );
